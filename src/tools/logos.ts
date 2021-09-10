c4163fe4a87f0a01ebaa4df2746a41afc3194178
bbenejc <bbe.nejc@gmail.com>
2021-09-10 12:40:38 +0200

    Add index icons

--- src/tools/logos.ts
+++ src/tools/logos.ts
@@ -1,4 +1,7 @@
 export function getStockLogoUrl(stock: string, dark = false): string {
+  if (stock.length > 3)
+    return "https://tornsy.com/images/" + stock.toUpperCase() + ".svg";
+
   return (
     "https://www.torn.com/images/v2/stock-market/" +
     (dark ? "dark-mode/" : "") +
