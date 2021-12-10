fcd1eb41e44281e1b70da5ea0e171329f581b71b
bbenejc <bbe.nejc@gmail.com>
2021-12-10 02:00:46 +0100

    Make placeholder width adjustable

--- src/components/placeholder/placeholder.tsx
+++ src/components/placeholder/placeholder.tsx
@@ -1,9 +1,10 @@
 import { CSSProperties, ReactElement } from 'react';
 import css from './placeholder.module.css';
 
-export function Placeholder({ className, style }: TProps): ReactElement {
+export function Placeholder({ className, style = {}, width }: TProps): ReactElement {
   const classNames = [css.Placeholder];
   if (className) classNames.push(className);
+  if (width) style.width = width + 'em';
 
   return <div className={classNames.join(' ')} style={style} />;
 }
@@ -11,4 +12,5 @@ export function Placeholder({ className, style }: TProps): ReactElement {
 type TProps = {
   className?: string;
   style?: CSSProperties;
+  width?: number;
 };
