636bade55aab82d552b4d55d1bd671f0a86bdac4
bbenejc <bbe.nejc@gmail.com>
2021-12-25 02:13:14 +0100

    iPhone PWA fixes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,7 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import { useSelector, useStore } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { use100vh } from 'react-div-100vh';
 import { useStocks, useQueryParams } from 'hooks';
 import { selectTheme, setOnline, setVisibility } from 'app/store';
 import { SMALL } from 'config';
@@ -11,13 +11,17 @@ import './app.css';
 export function App(): ReactElement {
   const store = useStore();
   const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
   const theme = useSelector(selectTheme);
-  const height = use100vh() || 0;
 
-  // window resize handler
   useEffect(() => {
-    const onResize = () => {
-      setWidth(window.innerWidth);
+    const onResize = (e: Event) => {
+      setTimeout(() => {
+        ReactDOM.unstable_batchedUpdates(() => {
+          setWidth(window.innerWidth);
+          setHeight(window.innerHeight);
+        });
+      }, 10);
     };
     const onOnline = () => store.dispatch(setOnline(window.navigator.onLine));
     const onVisibility = () => {
@@ -27,15 +31,20 @@ export function App(): ReactElement {
     window.addEventListener('online', onOnline);
     window.addEventListener('offline', onOnline);
     window.addEventListener('resize', onResize);
+    window.addEventListener('orientationchange', onResize);
     document.addEventListener('visibilitychange', onVisibility);
 
     return () => {
       window.removeEventListener('online', onOnline);
       window.removeEventListener('offline', onOnline);
       window.removeEventListener('resize', onResize);
+      window.removeEventListener('orientationchange', onResize);
       document.removeEventListener('visibilitychange', onVisibility);
     };
   }, [store]);
+
+  const top = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--sab'));
+  const bottom = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--sab'));
 
   const appCss = ['App', theme];
   const isSmall = width < SMALL;
@@ -47,7 +56,7 @@ export function App(): ReactElement {
       <div className={appCss.join(' ')} style={{ height }}>
         <div className="Main">
           <Menu width={width} height={height} />
-          {height > 0 && <Chart height={height - 40} width={isSmall ? width : width - 350} />}
+          {height > 0 && <Chart height={height - 40 - bottom / 2} width={isSmall ? width : width - 350} />}
         </div>
         {height > 0 && !isSmall && <Watchlist />}
         <Status />
